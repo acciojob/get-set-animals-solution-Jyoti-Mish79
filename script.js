991d5e1a59ef1187d7507e6d6728337e5ccd2aee
Jyoti-Mish79 <jyotimishra2996@gmail.com>
2023-03-20 17:27:01 +0530

    soliyt

--- script.js
+++ script.js
@@ -8,23 +8,26 @@ class Animal {
   }
 
   makeSound() {
-    console.log("The " + this.species + " makes a sound");
+    console.log('The animal makes a sound');
   }
 }
 
 class Cat extends Animal {
   purr() {
-    console.log("purr");
+    console.log('purr');
+  }
+
+  makeSound() {
+    console.log('Meow');
   }
 }
 
 class Dog extends Animal {
   bark() {
-    console.log("woof");
+    console.log('woof');
+  }
+
+  makeSound() {
+    console.log('Bark');
   }
 }
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
